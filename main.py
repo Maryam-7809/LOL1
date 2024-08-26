f83644108f21bb8e32a0777372bf568f6dedefa6
Maryam-7809 <itxkhan747@gmail.com>
2024-08-26 07:30:23 +0500

    main.py

--- main.py
+++ main.py
@@ -1,34 +1,3 @@
-from flask import Flask, request, send_from_directory, jsonify
-import requests
-import os
-import time
-
-app = Flask(__name__)
-
-# Path to your HTML file
-HTML_FILE_PATH = os.path.join('static', 'index.html')
-
-@app.route('/')
-def index():
-    return send_from_directory('static', 'index.html')
-
-@app.route('/send-message', methods=['POST'])
-def send_message():
-    access_token = request.form.get('accessToken')
-    inbox_target_id = request.form.get('inboxTargetID')
-    message = request.form.get('message')
-    timer = int(request.form.get('timer'))
-
-    # Simulate timer delay
-    time.sleep(timer)
-
-    # Send message to Facebook inbox
-    response = send_facebook_message(access_token, inbox_target_id, message)
-
-    if response.get('error'):
-        return jsonify({'error': response['error']['message']}), 400
-    return 'Message sent successfully'
-
 def send_facebook_message(access_token, recipient_id, message):
     GRAPH_API_URL = 'https://graph.facebook.com/v12.0/me/messages'
     headers = {
@@ -40,7 +9,5 @@ def send_facebook_message(access_token, recipient_id, message):
         'message': {'text': message},
     }
     response = requests.post(GRAPH_API_URL, headers=headers, json=payload)
+    print(response.json())  # Print the response to debug
     return response.json()
-
-if __name__ == '__main__':
-    app.run(debug=True)
